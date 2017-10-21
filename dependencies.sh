f0f0dd47d110690a20286465b2d20ffb8ebf9a99
Len <a78965@gmail.com>
2017-10-21 20:54:38 +0800

    Remove the npm install and add dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install NPM
+
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get update
+apt-get install -y nodejs
--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install NPM
-
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt-get update
-apt-get install -y nodejs
-
 # Initialize the git submodule
 git submodule init
 git submodule update
