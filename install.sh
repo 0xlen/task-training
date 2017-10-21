f8fc9958d21420226667f5fd834f074bad2d4cbd
Len <a78965@gmail.com>
2017-10-21 20:49:32 +0800

    Add automatic testing settings

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install NPM
+
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get update
+apt-get install -y nodejs
+
 # Initialize the git submodule
 git submodule init
 git submodule update
